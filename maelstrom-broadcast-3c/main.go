1564721843281d54c85a1710cdc8eb95a2d43057
Vineeth <vineethrajesh14@gmail.com>
2025-10-12 22:06:55 -0400

    3c broken

---
+++ maelstrom-broadcast-3c/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func gossip(message int, messageStore map[int]struct{}, topology map[string][]string, n *maelstrom.Node, msgID int, retrySet map[int]retryMessage) {
+	// if message has not been seen
+	if _, ok := messageStore[message]; !ok {
+		// store
+		messageStore[message] = struct{}{}
+
+		// forward to neighbors
+		for _, nei := range topology[n.ID()] {
+			retrySet[msgID] = retryMessage{
+				"gossip",
+				message,
+			}
+			// go func() {
+			// for _, ok := retrySet[msgID]; ok; {
+			// time.Sleep(time.Millisecond * 100)
+
+			// fmt.Fprintf(os.Stderr,"MSG_ID GOSSIPING %s %s", msgID,
+			n.Send(nei, map[string]any{
+				"type":    "gossip",
+				"message": message,
+				"msg_id":  msgID,
+			})
+			// }
+			// }()
+		}
+
+	}
+}
+
+type retryMessage struct {
+	Type    string
+	message int
+}
+
+func main() {
+	n := maelstrom.NewNode()
+
+	// seenMessage := make(map[uniqueMsgID]struct{})
+	messageStore := make(map[int]struct{})
+	topology := make(map[string][]string)
+	retrySet := make(map[int]retryMessage)
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+		}
+
+		message := int(body["message"].(float64))
+		defer gossip(message, messageStore, topology, n, int(body["msg_id"].(float64)), retrySet)
+
+		err := n.Reply(msg, map[string]any{
+			"type": "broadcast_ok",
+		})
+		for err != nil {
+			err = n.Reply(msg, map[string]any{
+				"type": "broadcast_ok",
+			})
+		}
+		return nil
+	})
+
+	n.Handle("gossip", func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+		}
+		message := int(body["message"].(float64))
+
+		// its not receiving gossip ok
+		gossip(message, messageStore, topology, n, int(body["msg_id"].(float64)), retrySet)
+
+		return n.Reply(msg, map[string]any{
+			"type":   "gossip_ok",
+			"msg_id": int(body["msg_id"].(float64)),
+		})
+	})
+
+	n.Handle("gossip_ok", func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+		}
+
+		msgID := int(body["msg_id"].(float64))
+		delete(retrySet, msgID)
+		return nil
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body struct {
+			Type     string              `json:"type"`
+			Topology map[string][]string `json:"topology"`
+		}
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology = body.Topology
+
+		return n.Reply(msg, map[string]string{"type": "topology_ok"})
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		messages := make([]int, 0, len(messageStore))
+		for msg := range messageStore {
+			messages = append(messages, msg)
+		}
+		if msgType, ok := body["type"].(string); ok && msgType == "read" {
+			body["messages"] = messages
+			body["type"] = "read_ok"
+		}
+
+		err := n.Reply(msg, body)
+		for err != nil {
+			err = n.Reply(msg, body)
+		}
+
+		return nil
+	})
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
